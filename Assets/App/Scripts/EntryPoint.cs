037bce044dddb55c543ab7523beec27cd398f4ee
eyedropsP <eyedropsp@gmail.com>
2021-04-28 01:07:05 +0900

    feat : EntryPoint.cs エントリーポイント追加

---
+++ Assets/App/Scripts/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using Akeruna.StateBehaviour;
+using UniFsm;
+using UnityEngine;
+
+namespace Akeruna
+{
+	public class EntryPoint : MonoBehaviour
+	{
+		private StateMachine<GameState> stateMachine;
+
+		private void Start()
+		{
+			stateMachine = new StateMachine<GameState>(GameState.Title);
+			
+			stateMachine.RegisterStateBehaviour(GameState.Title, new TitleStateBehaviour());
+			stateMachine.RegisterStateBehaviour(GameState.Prologue, new PrologueStateBehaviour());
+			stateMachine.RegisterStateBehaviour(GameState.Game, new MainStateBehaviour());
+			stateMachine.RegisterStateBehaviour(GameState.Epilogue, new EpilogueStateBehaviour());
+			stateMachine.RegisterStateBehaviour(GameState.Config, new ConfigStateBehaviour());
+		}
+
+		private void Update()
+		{
+			stateMachine.Tick();
+		}
+
+		private void OnDestroy()
+		{
+			stateMachine.Dispose();
+		}
+	}
+}
